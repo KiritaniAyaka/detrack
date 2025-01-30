46425ec91fcfd0f9d17bd73463887612f6531aaa
Ayaka <KiritaniAyaka@users.noreply.github.com>
2025-01-30 15:27:31 +0800

    feat: gracefully teardown deno kv

--- cleaner/cache.ts
+++ cleaner/cache.ts
@@ -1,11 +1,26 @@
+/// <reference lib="deno.unstable" />
+
 import { URLCleanerError } from "./error.ts";
 
-const kv = await Deno.openKv();
+const kv = await Deno.openKv("cleaner_cache.db");
+const ENABLE_CACHE = Deno.env.get("CACGE")?.startsWith("e");
 
 const CLEANER_CACHE_KEY = "cleaner_cache";
 const APP_VERSION = 1;
 
+const teardown = () => {
+  kv.close();
+  Deno.exit(0);
+};
+
+Deno.addSignalListener("SIGINT", teardown);
+if (Deno.build.os !== "windows") {
+  Deno.addSignalListener("SIGTERM", teardown);
+}
+
 export async function getCachedURL(url: string): Promise<string | null> {
+  if (!ENABLE_CACHE) return null;
+
   const key = [CLEANER_CACHE_KEY, APP_VERSION, url];
   const result = await kv.get<string>(key);
   return result.value;
