4f35da87d2ebbbaa5b13a504fd8de728fa8d2d25
Ayaka <KiritaniAyaka@users.noreply.github.com>
2025-01-31 22:56:20 +0800

    feat: random user agent

--- cleaner/rules/b23-redirector.ts
+++ cleaner/rules/b23-redirector.ts
@@ -1,10 +1,17 @@
+// @ts-types="npm:@types/user-agents@1.0.4"
+import UserAgent from "user-agents";
 import { RuleProcessor } from "./rule-type.ts";
 
 export const b23Redirector: RuleProcessor = {
   name: "b23Redirector",
   should: (url) => url.hostname === "b23.tv",
   process: async (url) => {
-    const response = await fetch(url, { redirect: "manual" });
+    const response = await fetch(url, {
+      redirect: "manual",
+      headers: {
+        "User-Agent": new UserAgent().toString(),
+      },
+    });
     if (response.status !== 302) return url;
 
     const location = response.headers.get("Location");
